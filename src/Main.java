9344cccf1ece7e5fa299913452498eb1dc7b8f0e
Netology Test <elen.1996@list.ru>
2024-12-14 19:46:18 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 1210;
+        int percent = 20;
+        int bonus = price / percent;
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
